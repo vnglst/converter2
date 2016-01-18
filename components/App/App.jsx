6f2334230d2a0be140344256b98e508e030aec7a
Koen van Gilst <koen.vangilst@gmail.com>
2016-01-18 16:17:00 +0100

    creates footer component

--- components/App/App.jsx
+++ components/App/App.jsx
@@ -31,8 +31,6 @@ export default class App extends React.Component {
 							toUnit="line" />
 					</Tab>
 	  		</Tabs>
-				<p>The Word to Line Rates Converter calculates word to line prices. A standard line (a German "Normzeile") is assumed to be 55 characters including spaces. The converter also converts per line to per word prices.</p>
-				<p>Charging per line is common practice in Germany. This may sound complicated and strange to translators used to word prices, but for many languages this is actually a much better and fairer way to estimate the amount of translation work involved, since the length of an average word varies greatly per language. Many agencies and clients outside Germay, however, still use word counts to estimate their prices. And that's where this conversion tool might come in handy!</p>
 			</div>
 		);
 	}
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,16 +1,18 @@
 //
 // Copyright (c) 2016 by Koen van Gilst (@vnglst) | MIT license
-// 
+//
 
 import React, { PropTypes } from 'react';
 import './Layout.scss';
 import Navigation from '../Navigation';
+import Footer from '../Footer';
 
 function Layout({ children }) {
   return (
     <div className="Layout">
       <Navigation />
       {children}
+      <Footer />
     </div>
   );
 }
