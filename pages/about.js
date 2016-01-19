d54ccf4f1687473c43a74df15693221522c12929
Koen van Gilst <koen.vangilst@gmail.com>
2016-01-19 20:34:12 +0100

    adds Tweets/testimonials

--- pages/about.js
+++ pages/about.js
@@ -33,6 +33,16 @@ export default class extends Component {
           <li>Add target languages</li>
           <li className="strikethrough">Make source code available on Github</li>
         </ul>
+
+        <h2>What people are saying</h2>
+        <blockquote className="twitter-tweet" lang="nl"><p lang="en" dir="ltr">Word to Line Rates - Useful tool for German translators who often charge per line <a href="http://t.co/GtwYso9EIo">http://t.co/GtwYso9EIo</a> <a href="https://twitter.com/hashtag/xl8?src=hash">#xl8</a></p>&mdash; Alessandra Vita (@AVInterpreter) <a href="https://twitter.com/AVInterpreter/status/654233874935975936">14 oktober 2015</a></blockquote>
+
+        <blockquote className="twitter-tweet" lang="nl"><p lang="en" dir="ltr">Word to Line Rates: useful conversion tool for translators <a href="http://t.co/gd5IV1rK2k">http://t.co/gd5IV1rK2k</a> <a href="https://twitter.com/hashtag/business?src=hash">#business</a> <a href="https://twitter.com/hashtag/xl8?src=hash">#xl8</a></p>&mdash; Giorgia Garavini (@GioGaravini) <a href="https://twitter.com/GioGaravini/status/655001270269399040">16 oktober 2015</a></blockquote>
+
+        <blockquote className="twitter-tweet" lang="nl"><p lang="en" dir="ltr">First time using <a href="https://twitter.com/vnglst">@vnglst</a>&#39;s line to word rate converter for a quote this morning - warmly recommended! <a href="http://t.co/oIRUuYd4fN">http://t.co/oIRUuYd4fN</a> <a href="https://twitter.com/hashtag/xl8?src=hash">#xl8</a></p>&mdash; Leonie Wagener (@l_wagener) <a href="https://twitter.com/l_wagener/status/542261020476321792">9 december 2014</a></blockquote>
+
+        <blockquote className="twitter-tweet" lang="nl"><p lang="en" dir="ltr">Awesome tool for <a href="https://twitter.com/hashtag/translators?src=hash">#translators</a>! Word to line <a href="https://twitter.com/hashtag/rate?src=hash">#rate</a> converter! <a href="http://t.co/tKHdeKpKwY">http://t.co/tKHdeKpKwY</a></p>&mdash; Martina@MovingWords (@mrMovingWords) <a href="https://twitter.com/mrMovingWords/status/654239049079746560">14 oktober 2015</a></blockquote>
+
       </div>
     );
   }
