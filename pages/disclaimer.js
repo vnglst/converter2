6b5e85282d0ea22ea14fcb5fd53d0ce5145235c4
Koen van Gilst <koen.vangilst@gmail.com>
2016-01-20 13:45:53 +0100

    adds disclaimer

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -9,7 +9,20 @@ export default class extends Component {
   render() {
     return (
       <div className="Text-Content">
-        <h1>Legal notice</h1>
+
+        <h1>Disclaimer</h1>
+        <p>
+          This website converts per word to per line rates based (among others) on the language conversion metrics provided by <a href="http://www.wolframalpha.com/input/?i=1000+words+in+English">Wolfram Alpha</a>.
+        </p>
+        <p>
+          The number of characters per word varies greatly per language. Wolfram Alpha calculates these values based on a statistical analysis of general texts and this might be different for texts in other areas. I therefore advise anyone to use this tool with caution.
+        </p>
+
+        <p>
+          You can verify your own results by using Microsoft Word or a CAT tool (like MemoQ or Trados).
+        </p>
+
+        <h2>Legal notice</h2>
         <h2>Responsible for site content:</h2>
         <p>
           Koen van Gilst<br />
@@ -20,6 +33,7 @@ export default class extends Component {
           <strong>Email:</strong> converter@vangilst.de<br />
           <strong>VAT number:</strong> NL 189376004 B02
         </p>
+
         <h2>External links:</h2>
         <p>
           Despite regular and diligent checking, I accept no responsibility for the content of direct or indirect external links. The operators of these sites are solely responsible for their content.
