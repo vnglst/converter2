a60fd26cd6714557143fe307a2592a3726314ee1
Koen van Gilst <koen.vangilst@gmail.com>
2016-02-07 12:05:58 +0100

    simplifies MoreRatesBox component

--- components/App/Converter/MoreRatesBox/MoreRatesBox.jsx
+++ components/App/Converter/MoreRatesBox/MoreRatesBox.jsx
@@ -3,20 +3,20 @@
 //
 
 import React from 'react';
-
-import PagePriceOutput from './PriceOutput/PagePriceOutput.jsx';
-import CharPriceOutput from './PriceOutput/CharPriceOutput.jsx';
-import HourPriceOutput from './PriceOutput/HourPriceOutput.jsx';
-import DayPriceOutput from './PriceOutput/DayPriceOutput.jsx';
+import PriceOutput from '../PriceOutput.jsx';
 
 export default class MoreRatesBox extends React.Component {
   render() {
+		const charPriceStr = this.props.priceOutput.charPriceStr;
+		const pagePriceStr = this.props.priceOutput.pagePriceStr;
+		const hourPriceStr = this.props.priceOutput.hourPriceStr;
+		const dayPriceStr = this.props.priceOutput.dayPriceStr;
     return (
       <form className="form-horizontal">
-        <PagePriceOutput {...this.props}/>
-        <CharPriceOutput {...this.props}/>
-        <HourPriceOutput {...this.props}/>
-        <DayPriceOutput {...this.props}/>
+        <PriceOutput {...this.props} priceStr={charPriceStr} priceLabel="Per character" />
+        <PriceOutput {...this.props} priceStr={pagePriceStr} priceLabel="Per page" />
+        <PriceOutput {...this.props} priceStr={hourPriceStr} priceLabel="Per hour" />
+        <PriceOutput {...this.props} priceStr={dayPriceStr} priceLabel="Per day" />
       </form>
     )
   }
