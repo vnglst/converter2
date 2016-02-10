eae59c6373ef9d0c059a7a9f84472a0f6d1bd4d5
Koen van Gilst <koen.vangilst@gmail.com>
2016-02-10 12:08:42 +0100

    replaces bootstrap with bootswatch flatly

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -7,7 +7,8 @@ import GoogleAnalytics from '../GoogleAnalytics';
 import Fonts from '../Fonts';
 import config from '../../config';
 
-const bootstrapCND = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+const bootstrapCDN = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+const bootswatchCDN = 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/flatly/bootstrap.min.css';
 
 function Html({ title, description, body, debug }) {
   return (
@@ -18,14 +19,16 @@ function Html({ title, description, body, debug }) {
         <title>{title || config.title}</title>
         <meta name="description" content={description || config.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href={bootstrapCND} media="bogus-real-load-below"/>
+
+        <link rel="stylesheet" href={bootswatchCDN} media="bogus-real-load-below"/>
         <script src={'/app.js?' + new Date().getTime()} />
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: body }} />
         <GoogleAnalytics />
         <Fonts />
-        <link rel="stylesheet" href={bootstrapCND} />
+
+        <link rel="stylesheet" href={bootswatchCDN} />
       </body>
     </html>
   );
