428e6d9fe3b997ccff3a6d887470de42c34d0496
Koen van Gilst <koen.vangilst@gmail.com>
2016-02-10 10:58:02 +0100

    Adds definition of a character

--- components/App/Converter/Settings/Settings.jsx
+++ components/App/Converter/Settings/Settings.jsx
@@ -9,13 +9,17 @@ export default (props) => (
   <div className="well">
     <div className="row">
       <div className="col-sm-6">
-      <SettingsInput {...props} _changeSetting={props._changeCharsPerLine} priceStr={props.charsPerLine} priceLabel="Chars/line"/>
-      <SettingsInput {...props} _changeSetting={props._changeCharsPerPage} priceStr={props.charsPerPage} priceLabel="Chars/page"/>
+      <SettingsInput {...props} _changeSetting={props._changeCharsPerLine} priceStr={props.charsPerLine} priceLabel={charsPerLine} />
+      <SettingsInput {...props} _changeSetting={props._changeCharsPerPage} priceStr={props.charsPerPage} priceLabel={charsPerPage}/>
       </div>
       <div className="col-sm-6">
       <SettingsInput {...props} _changeSetting={props._changeWordsPerDay} priceStr={props.wordsPerDay} priceLabel="Words/day"/>
       <SettingsInput {...props} _changeSetting={props._changeHoursPerDay} priceStr={props.hoursPerDay} priceLabel="Hours/day"/>
       </div>
     </div>
+    <p><sup>*</sup> Including spaces and special characters/numbers.</p>
   </div>
 )
+
+const charsPerLine = <span>Chars/line<sup>*</sup></span>;
+const charsPerPage = <span>Chars/page<sup>*</sup></span>;
